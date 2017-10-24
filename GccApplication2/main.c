eabcb5c45d284363406e33f8b6e872f7685a0921
NopyJF <Nithichai@users.noreply.github.com>
2017-10-25 01:12:18 +0700

    Update by Nopy

--- GccApplication2/main.c
+++ GccApplication2/main.c
@@ -4,85 +4,32 @@
  * Created: 9/27/2017 12:52:16 PM
  * Author : nithi
  */
+#define F_CPU 16000000L
+#define BAUD 9600
+#define MYUBRR ((F_CPU/16/BAUD)-1)
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include FOSC 16000000
-#define BAUD 9600
-#define MYUBRR ((FOSC/16/BAUD)-1)
-volatile uint16_t time_push;
-volatile uint16_t Capt1;
+#include <util/delay.h>
+#include <stdlib.h>
+#include <math.h>
+
+volatile uint16_t length;
+// volatile uint16_t Capt1;
 /*
  * Flag 0 = Send trigger
  * Flag 1 = Detect rising
  * Flag 2 = Count time and detect falling
  * Flag 3 = Send to computer
- * Flag 4 = Ack from computer and reset
+ * Flag 4 = Reset
  */
-volatile uint8_t Flag = 0;
-
-void SetFlag0 (void)
-{
-	/* Set normal mode */
-	TCCR1A = 0x00;
-	/* Set prescaler 8 */
-	TCCR1B = 0x02;
-	/* Interrupt timer overflow */
-	TIMSK1 = 0x01;
-	/* Set time 10 us */
-	TCNT1 = 65516;
-	/* Start interrupt */
-	sei();
-	/* Set portB is output */
-	DDRB = 0xFF;
-}
-
-void SetFlag1 (void) 
-{
-	/* Set normal mode */
-	TCCR1A = 0x00;
-	/* Set prescaler 1024 and set rising edge detect */
-	TCCR1B = 0x45;
-	/* Interrupt capture and timer overflow */
-	TIMSK1 = 0x21;
-	/* Start interrupt */
-	sei();
-	/* Set portB is output */
-	DDRB = 0x00;
-}
-
-void SetFlag2(void)
-{
-	/* Set normal mode */
-	TCCR1A = 0x00;
-	/* Set prescaler 1024 and set falling edge detect */
-	TCCR1B = 0x05;
-	/* Interrupt capture and timer overflow */
-	TIMSK1 = 0x21;
-	/* Start interrupt */
-	sei();
-	/* Set portB is output */
-	DDRB = 0x00;
-}
-
-void SetFlag3(void)
-{
-	/* Set normal mode */
-	TCCR1A = 0x00;
-	/* Set prescaler 1024 and set rising edge detect */
-	TCCR1B = 0x45;
-	/* Interrupt timer overflow */
-	TIMSK1 = 0x01;
-	/* Start interrupt */
-	sei();
-	/* Set portB is output */
-	DDRB = 0x00;
-} 
+volatile uint8_t Flag;
+volatile uint8_t triggerState;
 
 unsigned char USART_Receive(void) {
 	/* Wait for data to be received */
 	while ( !(UCSR0A & (1<<RXC0)) );
 	/* Get and return received data from buffer */
-	return UDR0; 
+	return UDR0;
 }
 
 void USART_Transmit(unsigned char data) {
@@ -92,7 +39,7 @@ void USART_Transmit(unsigned char data) {
 	UDR0 = data;
 }
 
-void USART_Init( unsigned int ubrr) {
+void USART_Init(unsigned int ubrr) {
 	/*Set baud rate */
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
@@ -100,53 +47,192 @@ void USART_Init( unsigned int ubrr) {
 	/* Enable receiver and transmitter */
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
 	/* Set frame format: 8data, 2stop bit */
-	UCSR0C = (3<<UCSZ00); 
+	UCSR0C = (3<<UCSZ00);
+}
+
+void Start(void)
+{
+	length = 0;
+	Flag = 0;
+	triggerState = 0;
+	TCCR1A = 0x00;
+	TIMSK1 = 0x21;
+	sei();
+}
+
+void RestartValue(void)
+{
+	length = 0;
+	Flag = 0;
+	triggerState = 0;	
+}
+
+/*  */
+void SendTrigger (void)
+{
+	//USART_Transmit('T');
+	//USART_Transmit('\n');
+	/* Set time 10 us */
+	TCNT1 = 65516;
+	/* Set prescaler 8 */
+	TCCR1B = 0x02;
+	/* Set portB is output */
+	DDRB = 0x02;
+	/* Set trigger high */
+	PORTB = 0x02;
+}
+
+void DetectRising (void) 
+{
+	// USART_Transmit('R');
+	// USART_Transmit('\n');
+	/* Set time = */
+	TCNT1 = 0;
+	/* Set normal mode */
+	// TCCR1A = 0x00;
+	/* Set prescaler 1024 and set rising edge detect */
+	TCCR1B = 0x45;
+	/* Interrupt capture */
+	// TIMSK1 = 0x21;
+	/* Start interrupt */
+	// sei();
+}
+
+void DetectFalling(void)
+{
+	//USART_Transmit('F');
+	//USART_Transmit('\n');
+	/* Set time = */
+	TCNT1 = 0;
+	/* Set normal mode */
+	//TCCR1A = 0x00;
+	/* Set prescaler 1024 and set falling edge detect */
+	TCCR1B = 0x05;
+	/* Interrupt capture */
+	// TIMSK1 = 0x21;
+	/* Start interrupt */
+	//sei();
+}
+
+void SendAllNumber(volatile uint16_t number)
+{
+	int n = 5; //log10(number) + 1;
+	int i = 0;
+	int data[n];
+	for (i = 0; i < n; i++, number /= 10)
+	{
+		data[i] = number % 10;
+	}
+	for (i = n-1; i >= 0; i--)
+	{
+		USART_Transmit(data[i] + '0');
+	}
+	USART_Transmit('\n');
+}
+
+void SendValue(volatile uint16_t number)
+{
+	// USART_Transmit('V');
+	// USART_Transmit(':');
+	/* Set normal mode */
+	// TCCR1A = 0x00;
+	/* Set no clock and set falling edge detect */
+	TCCR1B = 0x00;
+	/* no interrupt */
+	TIMSK1 = 0x00;
+	SendAllNumber(number);
 }
 
 ISR(TIMER1_CAPT_vect) 
 {
 	if (Flag == 1)
 	{
-		SetFlag2();
-		Flag++;
+		/*USART_Transmit(':');
+		USART_Transmit('1');
+		USART_Transmit('\n');*/
+		DetectFalling();
+		Flag = 2;
 	}
-	if (Flag == 2)
+	else if (Flag == 2)
 	{
-		Capt1 = ICR1;
-		SetFlag3();
-		Flag++;
+		length = ICR1;
+		if (length < 64)
+		{
+			length = length * 1024 / 16 / 58;
+		} 
+		else if (length < 1024)
+		{
+			length = length / 16 * 1024 / 58;
+		}
+		else if (length < 3712)
+		{
+			length = length / 58 * 1024 / 16;
+		} 
+		else 
+		{ 
+			length = length / 16 / 58 * 1024;
+		}
+		length = ICR1; //ICR1 * 1024 / 16 / 58;
+		/*USART_Transmit(':');
+		USART_Transmit('2');
+		USART_Transmit('\n');*/
+		SendValue(length);
+		_delay_ms(500);
+		Start();
+		SendTrigger();
+	} 
+	else 
+	{
+		USART_Transmit('C');
+		USART_Transmit('A');
+		USART_Transmit('P');
+		USART_Transmit('T');
+		USART_Transmit('>');
+		USART_Transmit(Flag + '0');
+		USART_Transmit('\n');
+		Start();
+		SendTrigger();
 	}
 }
 
 ISR(TIMER1_OVF_vect) 
 {
+	/*USART_Transmit('O');
+	USART_Transmit('V');
+	USART_Transmit('F');*/
 	if (Flag == 0) 
 	{
-		SetFlag1();
-		Flag++;
+		/*USART_Transmit(':');
+		USART_Transmit('0');
+		USART_Transmit('\n');*/
+		PORTB = 0x00;
+		triggerState = 1;
+		DetectRising();
+		Flag = 1;
+	} 
+	else 
+	{
+		USART_Transmit('O');
+		USART_Transmit('V');
+		USART_Transmit('F');
+		USART_Transmit('>');
+		USART_Transmit('X');
+		USART_Transmit('\n');
+		Start();
+		SendTrigger();
 	}
 }
 
 int main(void)
 {	
-	SetFlag0();
+	// DelayRestart();
+	Start();
 	USART_Init(MYUBRR);
+	SendTrigger();
 	while(1) {
-		if (Flag == 3) 
-		{
-			time_push = Capt1;
-			USART_Transmit((char) time_push);
-			char recv = USART_Receive();
-			if (recv == 'O') 
-			{
-				Flag++;
-			}
-		}
-		if (Flag == 4)
-		{
-			Capt1 = 0;
-			Flag = 0;
-			SetFlag0();
-		}
+		/*USART_Transmit(Flag + '0');
+		USART_Transmit(triggerState + '0');
+		USART_Transmit('\n');
+		_delay_ms(200);*/
 	}
 }
---
+++ README.txt
@@ -0,0 +1,2 @@
+C:\Program Files (x86)\Arduino\hardware\tools\avr/bin/avrdude 
+-C"C:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" -v -patmega328p -carduino -PCOM3 -b115200 -D -Uflash:w:"$(ProjectDir)Debug\$(TargetName).hex":i
